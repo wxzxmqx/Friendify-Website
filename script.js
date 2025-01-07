6111d5e6c6dbeaf15cb8e76a948dffd84650a483
Serhii Yatsunenko <yatsunenko.serhii@hneu.net>
2025-01-08 00:47:02 +0100

    built a website

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollToTopBtn.style.display = 'block'; 
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+};
+
+scrollToTopBtn.addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
